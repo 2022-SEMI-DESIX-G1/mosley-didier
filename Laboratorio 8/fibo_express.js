7602cb1171b2e2df1c18f467a24cde1bbc0b7c7a
didiermosley <65973585+didiermosley@users.noreply.github.com>
2022-05-28 23:34:48 -0500

    Add files via upload

--- Laboratorio 8/fibo_express.js
+++ Laboratorio 8/fibo_express.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
 
+let numb = 7;
+
+function fibonacci(num){
+    let n, number=0, nextNumber=1;
+    let arr = [];
+    for (let index = 1; index <=num; index++) {
+        arr.push(number);
+        console.log(arr);
+        n = number + nextNumber;
+        number = nextNumber;
+        nextNumber = n;
+    }
+    return arr;
+}
+
+
 
 app.get('/', function (req, res) {
     res.send("Test")
 })
 
-app.post('/other', function (req, res){
-    res.send("Test")
+app.post('/array', function (req, res){
+    res.json({
+        fibonacci: fibonacci(numb)
+    })
 })
 
 app.listen(3000)
